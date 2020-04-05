70f9d68c40b805fd80c10e543f9976eb28ffae9c
Chris Zuber <shgysk8zer0@gmail.com>
2020-04-04 17:42:44 -0700

    Fix endpoint for managing requests
    Changes got lost in a branch change. Re-wrote endpoint

--- contact/index.php
+++ contact/index.php
@@ -1,9 +1,10 @@
 <?php
 namespace Test;
-use \shgysk8zer0\{User, Person};
+use \shgysk8zer0\{User, Person, Message};
 use \shgysk8zer0\PHPAPI\{API, PDO, Headers, HTTPException, UUID};
 use \shgysk8zer0\PHPAPI\Abstracts\{HTTPStatusCodes as HTTP};
 use \Throwable;
+use const \Consts\{HMAC_KEY};
 
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'autoloader.php';
 
@@ -12,45 +13,80 @@ try {
 
 	$api->on('GET', function(API $req): void
 	{
-		throw new HTTPException('Not implemented', HTTP::NOT_IMPLEMENTED);
+		if ($req->get->has('token', 'uuid')) {
+			$pdo = PDO::load();
+			$user = new User($pdo, HMAC_KEY);
+
+			if (! $user->loginWithToken($req->get)) {
+				throw new HTTPException('Invalid or expired token', HTTP::UNAUTHORIZED);
+			} elseif (! $user->can('viewMessage')) {
+				throw new HTTPException('You do not have permission to do that', HTTP::FORBIDDEN);
+			} elseif ($msg = Message::getFromIdentifier($pdo, $req->get->get('uuid', false))) {
+				$msg->markAsRead($pdo, true);
+				Headers::contentType('application/json');
+				echo json_encode($msg);
+			} else {
+				throw new HTTPException('No messages found', HTTP::NOT_FOUND);
+			}
+		} elseif ($req->get->has('token')) {
+			// Get messages list
+			$pdo = PDO::load();
+			$user = new User($pdo, HMAC_KEY);
+
+			if (! $user->loginWithToken($req->get)) {
+				throw new HTTPException('Invalid or expired token', HTTP::UNAUTHORIZED);
+			} elseif (! $user->can('viewMessage')) {
+				throw new HTTPException('You do not have permission to do that', HTTP::FORBIDDEN);
+			} elseif ($msgs = Message::fetchAll($pdo, $req->get->get('offset', false, 0))) {
+				Headers::contentType('application/json');
+				echo json_encode($msgs);
+			} else {
+				throw new HTTPException('No messages found', HTTP::NOT_FOUND);
+			}
+		} else {
+			throw new HTTPException('Missing token in request', HTTP::BAD_REQUEST);
+		}
 	});
 
 	$api->on('POST', function(API $req): void
 	{
 		if ($req->post->has('identifier')) {
 			throw new HTTPException('Not implemented', HTTP::NOT_IMPLEMENTED);
-		} elseif ($req->post->has('name', 'email', 'telephone', 'message') and filter_var($req->post->get('email'), FILTER_VALIDATE_EMAIL)) {
-			$stm = PDO::load()->prepare('INSERT INTO `messages` (
-				`identifier`,
-				`name`,
-				`email`,
-				`telephone`,
-				`message`
-			) VALUES (
-				:uuid,
-				:name,
-				:email,
-				:telephone,
-				:message
-			);');
-
-			if ($stm->execute([
-				'uuid'      => new UUID(),
-				'name'      => $req->post->get('name'),
-				'email'     => $req->post->get('email'),
-				'telephone' => $req->post->get('telephone'),
-				'message'   => $req->post->get('message'),
-			]) and $stm->rowCount() === 1) {
+		} elseif (! $req->post->has('name', 'email', 'telephone', 'subject', 'message')) {
+			throw new HTTPException('Missing required inputs', HTTP::BAD_REQUEST);
+		} elseif ($msg = Message::createFromUserInput($req->post) and $msg->valid()) {
+			if ($uuid = $msg->save(PDO::load())) {
 				Headers::status(HTTP::CREATED);
+				Headers::contentType('application/json');
+				echo json_encode(['identifier' => $uuid]);
 			} else {
-				throw new HTTPException('Error saving message', HTTP::INTERNAL_SERVER_ERROR);
+				throw new HTTPException('Error saving message to database', HTTP::INTERNAL_SERVER_ERROR);
 			}
+		} else {
+			throw new HTTPException('An unknown error occured saving message', HTTP::INTERNAL_SERVER_ERROR);
 		}
 	});
 
 	$api->on('DELETE', function(API $req): void
 	{
-		throw new HTTPException('Not implemented', HTTP::NOT_IMPLEMENTED);
+		$pdo = PDO::load();
+		$user = new User($pdo, HMAC_KEY);
+
+		if (! $req->get->has('token', 'uuid')) {
+			throw new HTTPException('Missing UUID or token', HTTP::BAD_REQUEST);
+		} elseif (! $user->loginWithToken($req->get)) {
+			throw new HTTPException('Invalid or expired token', HTTP::UNAUTHORIZED);
+		} elseif (! $user->can('deleteMessage')) {
+			throw new HTTPException('You do not have permission to do that', HTTP::FORBIDDEN);
+		} elseif ($msg = Message::getFromIdentifier($pdo, $req->get->get('uuid'))) {
+			if ($msg->delete($pdo)) {
+				Headers::status(HTTP::NO_CONTENT);
+			} else {
+				throw new HTTPException('Error deleting message', HTTP::INTERNAL_SERVER_ERROR);
+			}
+		} else {
+			throw new HTTPException('Message not found', HTTP::NOT_FOUND);
+		}
 	});
 
 	$api();
